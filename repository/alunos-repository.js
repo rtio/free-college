20a793734f7d9b4f06a08458014362e2bd073917
rtio <meneses.tio@gmail.com>
2023-11-06 20:24:06 -0300

    mon

--- repository/alunos-repository.js
+++ repository/alunos-repository.js
@@ -1,8 +1,28 @@
 var conn = require('./conn');
 
 async function getAlunos() {
+    return execSql('SELECT id, nome, email FROM Alunos', []);
+}
+
+async function getAluno(id) {
+    return execSql('SELECT id, nome, email FROM Alunos WHERE id = ?', [id]);
+}
+
+async function addAluno(aluno) {
+    return execSql('INSERT INTO Alunos (nome, email) VALUES (?, ?)', [aluno.nome, aluno.email]);
+}
+
+async function deleteAluno(id) {
+    return execSql('DELETE FROM Alunos WHERE id = ?', [id]);
+}
+
+async function editAluno(aluno) {
+    return execSql('UPDATE Alunos SET nome = ?, email = ? WHERE id = ?', [aluno.nome, aluno.email, aluno.id]);
+}
+
+async function execSql(sql, params) {
     return new Promise((resolve, reject)=>{
-        conn.connection.query('SELECT id, nome, email FROM Alunos', function (error, results, fields) {
+        conn.connection.query(sql, params, function (error, results, fields) {
             if (error) {
                 return reject(error);
             }
@@ -12,5 +32,9 @@ async function getAlunos() {
 }
 
 module.exports = {
-    getAlunos
+    getAlunos,
+    addAluno,
+    getAluno,
+    deleteAluno,
+    editAluno
 }
--- routes/alunos.js
+++ routes/alunos.js
@@ -1,10 +1,74 @@
-const { getAlunos } = require('../repository/alunos-repository');
+const { getAlunos, addAluno, getAluno, deleteAluno, editAluno } = require('../repository/alunos-repository');
 const express = require('express')
 const router = express.Router()
 
 // GET /alunos
+// curl --request GET \
+//  --url http://localhost:3000/alunos \
+//  --header 'User-Agent: insomnia/8.2.0'
 router.get('/', async (req, res) => {
     res.json(await getAlunos());
+});
+
+// GET /alunos/:id
+// curl --request GET \
+//   --url http://localhost:3000/alunos/15 \
+//   --header 'User-Agent: insomnia/8.2.0'
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    const aluno = await getAluno(id);
+    console.log(aluno);
+    if (aluno.length > 0) {
+        res.json(aluno[0]);
+    } else {
+        res.status(404);
+        res.json();
+    }
+});
+
+// POST /alunos
+// curl --request POST \
+//   --url http://localhost:3000/alunos \
+//   --header 'Content-Type: application/json' \
+//   --header 'User-Agent: insomnia/8.2.0' \
+//   --data '{
+// 	"nome": "Jack2",
+// 	"email": "jack@mail.com"
+// }'
+router.post('/', async (req, res) => {
+    const aluno = req.body;
+    const result = await addAluno(aluno);
+    const newAluno = await getAluno(result.insertId);
+
+    res.status(201);
+    res.json(newAluno[0]);
+});
+
+// DELETE /alunos/:id
+// curl --request DELETE \
+//   --url http://localhost:3000/alunos/16 \
+//   --header 'User-Agent: insomnia/8.2.0'
+router.delete('/:id', async (req, res) => {
+    await deleteAluno(req.params.id);
+    res.status(204);
+    res.json();
+});
+
+// UPDATE /alunos/:id
+// curl --request PUT \
+//   --url http://localhost:3000/alunos/14 \
+//   --header 'Content-Type: application/json' \
+//   --header 'User-Agent: insomnia/8.2.0' \
+//   --data '	{
+// 		"nome": "Jack4",
+// 		"email": "jack4@mail.com"
+// 	}'
+router.put('/:id', async (req, res) => {
+    const aluno = req.body;
+    aluno.id = req.params.id;
+    await editAluno(aluno);
+    const editedAluno = await getAluno(aluno.id);
+    res.json(editedAluno);
 });
 
 module.exports = router;
