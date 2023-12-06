c34ad5357a06ab0b40a32afd5239749ac4b51ec9
rtio <meneses.tio@gmail.com>
2023-12-06 20:11:01 -0300

    add migrations

---
+++ seeders/20231206230235-demo-user.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     */
+    await queryInterface.bulkInsert('Users', [{
+      first_name: 'John',
+      last_name: 'Doe',
+      email: 'john.doe@mail.com',
+      password: '123456',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     */
+    await queryInterface.bulkDelete('Users', null, {});
+  }
+};
